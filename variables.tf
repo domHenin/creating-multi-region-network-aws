bf589590a37facd85d275954898ea205affe3c3a
clxdev <dominic.heningburg@cloudlogix.io>
2022-05-20 16:38:06 -0400

    development in progress

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "profile" {
+  default = "cloud_user"
+  type = string
+}
+
+variable "region_master" {
+  default = "us-east-1"
+  type = string
+}
+
+variable "region_worker" {
+    default = "us-west=2"
+    type = string
+}
